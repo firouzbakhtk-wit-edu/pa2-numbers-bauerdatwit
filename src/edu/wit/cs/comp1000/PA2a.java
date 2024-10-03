c5e3395efaafb6e80d13bb88b5bc840c50e09235
Dylan Bauer <bauerd@wit.edu>
2024-10-03 16:28:51 -0400

    PA2A and B have been added

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,114 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+/**
+ * PA2A Submission
+ * 
+ * When you run this code it should ask for five whole numbers.
+ * Once the five whole numbers are stored, it will calculate the sum of the positive numbers, negative numbers, and the sum of all numbers.
+ * The program will also find the average of the positive numbers, negative numbers, and all numbers.
+ * 
+ * @author Dylan Bauer
+ */
+
+import java.util.Scanner; // Imports scanner utility
+
 public class PA2a {
 
-	// TODO: document this function
+	/**
+	 * Creates a new scanner object an array for the 5 input ints.
+	 * There also also ints created for the total positive, negative, and total inputs.
+	 * Ints are also created for the number of positive and negative inputs.
+	 * Doubles are created for the averages of all, positive, and negative inputs.
+	 * 
+	 * @param args command-line arguments, ignored
+	 */
+	
 	public static void main(String[] args) {
-		// TODO: write your code here
+		
+		Scanner input = new Scanner(System.in);
+		
+		int[] userInputs = new int[5];
+		int totalPositive = 0;
+		int totalNegative = 0;
+		int totalAll = 0;
+		int numPositive = 0;
+		int numNegative = 0;
+		double avgPos = 0;
+		double avgNeg = 0;
+		double avgAll = 0;
+		
+		/**
+		 * Below, the program will print a prompt for entering five whole numbers and the program will store these numbers in the array created above.
+		 * It will also check each number in the array to see if it is positive or negative. It will then add them together to the either total positive or negative.
+		 * Plus, the program will add a tick for every time there is a new positive or negative number.
+		 */
+		
+		System.out.printf("Enter five whole numbers: ");
+		
+		for(int a = 0; a < 5; a++) {
+			userInputs[a] = input.nextInt();
+			totalAll += userInputs[a];
+				if(userInputs[a] > 0) {
+					totalPositive += userInputs[a];
+					numPositive++;
+				} else if(userInputs[a] <= 0) {
+					totalNegative += userInputs[a];
+					numNegative++;
+				}
+		}
+		
+		/**
+		 * This section calculates the average positive, negative, and the total average and store them in the proper variables.
+		 */
+		
+		if(numPositive > 0) {
+			avgPos = (double) totalPositive / numPositive;
+		}
+		
+		if(numNegative > 0) {
+			avgNeg = (double) totalNegative / numNegative;
+		}
+		
+		avgAll = (double) totalAll / 5;
+		
+		/**
+		 * This section will print the results. If statements have been incorporated in order to have the correct grammar (either plural or non-plural)
+		 */
+		
+		if(numPositive == 1) {
+			System.out.printf("The sum of the %d positive number: %d%n", numPositive, totalPositive);
+		}
+		else {
+			System.out.printf("The sum of the %d positive numbers: %d%n", numPositive, totalPositive);
+		}
+		
+		if(numNegative == 1) {
+			System.out.printf("The sum of the %d non-positive number: %d%n", numNegative, totalNegative);
+		}
+		else {
+			System.out.printf("The sum of the %d non-positive numbers: %d%n", numNegative, totalNegative);
+		}
+		
+		System.out.printf("The sum of the 5 numbers: %d%n", totalAll);
+		
+		if(numPositive == 1) {
+			System.out.printf("The average of the %d positive number: %.2f%n", numPositive, avgPos);
+		}
+		else {
+			System.out.printf("The average of the %d positive numbers: %.2f%n", numPositive, avgPos);
+		}
+		
+		if(numNegative == 1) {
+			System.out.printf("The average of the %d non-positive number: %.2f%n", numNegative, avgNeg);
+		}
+		else {
+			System.out.printf("The average of the %d non-positive numbers: %.2f%n", numNegative, avgNeg);
+		}
+		
+		System.out.printf("The average of the 5 numbers: %.2f%n", avgAll);
+		
+		input.close(); // Closes scanner input
+		
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,72 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+/**
+ * PA2B Submission
+ * 
+ * When you run this code it should ask for 3 inputs (a, b, and c). They will be stored as doubles and calculated through the quadratic formula.
+ * The program will let you know if the roots are imaginary. If the roots are not imaginary, it will either spit out the root or roots (depending)
+ * 
+ * @author Dylan Bauer
+ */
+
+import java.util.Scanner; // Imports scanner utility
+
 public class PA2b {
 
-	// TODO: document this function
+	/**
+	 * Creates 4 doubles, 3 for the inputs and one for the discriminant. It also creates a new scanner object for the user input.
+	 * It will then continue to collect the user inputs with a prompt in the console
+	 * 
+	 * @param args command-line arguments, ignored
+	 */
+	
 	public static void main(String[] args) {
-		// TODO: write your code here
+		
+		double input_a;
+		double input_b;
+		double input_c;
+		double disc;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.printf("Enter a b c: ");
+		input_a = input.nextDouble();
+		input_b = input.nextDouble();
+		input_c = input.nextDouble();
+		
+		disc = (input_b * input_b) - 4 * input_a * input_c; // Formula for discriminant
+		
+		/**
+		 * Below it will calculate the roots. If the discriminant is less than zero, it will output that the roots are imaginary.
+		 * If the discriminant is equal to zero it will only calculate and output the singular root.
+		 * If the discriminant is greater than 0 it will find and output both of the roots.
+		 */
+		
+		if(disc < 0) {
+			System.out.printf("Roots: imaginary%n");
+		}
+		
+		else if(disc == 0) {
+			disc = -input_b / (2 * input_a);
+			System.out.printf("Root: %.2f%n", disc);
+		}
+		
+		else {
+			double root1 = (-input_b + Math.sqrt(disc)) / (2 * input_a);
+			double root2 = (-input_b - Math.sqrt(disc)) / (2 * input_a);
+			
+			if(root1 > root2) {
+				double temp = root1;
+				root1 = root2;
+				root2 = temp;
+			}
+			
+			System.out.printf("Roots: %.2f, %.2f%n", root1, root2);
+			
+		}
+	
+		input.close(); // Closes scanner object
+		
 	}
 
 }
